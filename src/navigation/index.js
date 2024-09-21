4c9e7a88d05f0e37e463436a8a1179e941f03fe9
KeilaBarreto <keilakdbweb@gmail.com>
2024-09-21 11:28:02 -0300

    continue

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,20 +1,26 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import WelcomeScreen from '../screens/WelcomeScreen';
-import HomeScreen from '../screens/HomeScreen';
+import { View, Text } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+import WelcomeScreen from "../screens/WelcomeScreen";
+import RecipeDetailsScreen from "../screens/RecipeDetailsScreen";
 
 const Stack = createNativeStackNavigator();
 
-function AppNavigation() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Welcome" component={WelcomeScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Welcome"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="RecipeDetails" component={RecipeDetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-export default AppNavigation;
